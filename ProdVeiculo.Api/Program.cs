800322dd25bb8eace470452c747b1eaa884fa605
Tiago Ferraro Augusto <tiagoferraro@gmail.com>
2024-04-26 09:35:24 -0300

    Adição do swagger

--- ProdVeiculo.Api/Program.cs
+++ ProdVeiculo.Api/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using ProdVeiculo.Api.Filter;
 using ProdVeiculo.Infra.Ioc;
 using System.Text.Json.Serialization;
@@ -26,11 +27,16 @@ builder.Services.Configure<ApiBehaviorOptions>(o =>
     o.SuppressModelStateInvalidFilter = true;
 });
 
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Cadastro de Produto Fornecedor", Version = "v1" });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-
+app.UseSwagger();
+app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Cadastro de Produto Fornecedor v1"); });
 
 
 app.UseAuthorization();
--- ProdVeiculo.Test/DTO/ProdutoDTOTest.cs
+++ ProdVeiculo.Test/DTO/ProdutoDTOTest.cs
@@ -1,11 +1,4 @@
-﻿using ProdVeiculo.domain.entity.Enum;
-using ProdVeiculo.domain.entity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ProdVeiculo.Application.DTOs;
+﻿using ProdVeiculo.Application.DTOs;
 using System.ComponentModel.DataAnnotations;
 
 namespace ProdVeiculo.Test.DTO
